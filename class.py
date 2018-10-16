8be6b1d35bdba4859328e9afc93d35266b656ea8
SiGuaHa <1058435977@qq.com>
2018-10-16 17:36:13 +0800

    1. 2018-10-16study

---
+++ class.py
@@ -0,0 +1,60 @@
+# 面向对象最重要的概念就是类（Class）和实例（Instance），必须牢记类是抽象的模板，比如Student类，而实例是根据类创建出来的一个个具体的“对象”，
+# 每个对象都拥有相同的方法，但各自的数据可能不同。
+
+# 仍以Student类为例，在Python中，定义类是通过class关键字：
+# class后面紧接着是类名，即Student，类名通常是大写开头的单词，紧接着是(object)，表示该类是从哪个类继承下来的，继承的概念我们后面再讲，
+# 通常，如果没有合适的继承类，就使用object类，这是所有类最终都会继承的类。
+class Student(object):
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+    
+    def print_score(self):
+        print("%s: %s" % (self.__name, self.__score))
+
+    def get_grade(self):
+        if self.__score >= 90:
+            return 'A'
+        elif self.__score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
+    def get_name(self):
+        return self.__name
+
+    def get_name(self):
+        return self.__score
+
+    def set_score(self, score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            raise ValueError("bad score")
+
+# 注意：特殊方法“__init__”前后分别有两个下划线！！！
+# 注意到__init__方法的第一个参数永远是self，表示创建的实例本身，因此，在__init__方法内部，就可以把各种属性绑定到self，因为self就指向创建的实例本身。
+# 有了__init__方法，在创建实例的时候，就不能传入空的参数了，必须传入与__init__方法匹配的参数，但self不需要传，Python解释器自己会把实例变量传进去：
+
+bart = Student("Bart Simpson", 59)
+bart.print_score()
+
+
+# 在Python中，实例的变量名如果以__开头，就变成了一个私有变量（private），只有内部可以访问，外部不能访问，所以，我们把Student类改一改：
+# 在Python中，变量名类似__xxx__的，也就是以双下划线开头，并且以双下划线结尾的，是特殊变量，
+# 特殊变量是可以直接访问的，不是private变量，所以，不能用__name__、__score__这样的变量名。
+
+# 有些时候，你会看到以一个下划线开头的实例变量名，比如_name，这样的实例变量外部是可以访问的，
+# 但是，按照约定俗成的规定，当你看到这样的变量时，意思就是，“虽然我可以被访问，但是，请把我视为私有变量，不要随意访问”。
+
+# 双下划线开头的实例变量是不是一定不能从外部访问呢？其实也不是。
+# 不能直接访问__name是因为Python解释器对外把__name变量改成了_Student__name，所以，仍然可以通过_Student__name来访问__name变量：
+
+# >>> bart = Student('Bart Simpson', 59)
+# >>> bart.get_name()
+# 'Bart Simpson'
+# >>> bart.__name = 'New Name' # 设置__name变量！
+# >>> bart.__name
+# 'New Name'
+# 表面上看，外部代码“成功”地设置了__name变量，但实际上这个__name变量和class内部的__name变量不是一个变量！
+# 内部的__name变量已经被Python解释器自动改成了_Student__name，而外部代码给bart新增了一个__name变量。不信试试：
